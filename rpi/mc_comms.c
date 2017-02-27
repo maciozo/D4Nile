4f4c8efc2c0313e3d2fc727cb452166f12ead108
maciozo <maciek1663@gmail.com>
2017-02-27 16:31:28 +0000

    Formatting of received sensor data
    Wrote code that formats the sensor data received from the mc neatly in
    to a struct.

--- rpi/mc_comms.c
+++ rpi/mc_comms.c
@@ -4,13 +4,18 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/mman.h>
+#include <time.h>
 
 #include "mc_comms.h"
 #include "../constants.h"
 
-int main(void)
+recvBuffer = "";
+sendBuffer = "":
+sensorData = {0};
+
+int uartInit(const char* device)
 {
-    FILE *serial = fopen(SERIAL_DEVICE, "r+");
+    FILE *serial = fopen(device, "r+");
     
     /* Making fgets non-blocking 
     http://stackoverflow.com/a/6055774 */
@@ -21,15 +26,23 @@ int main(void)
     
     uartLoop(serial);
     fclose(serial);
+    
+    return 0;
 }
 
 int uartLoop(FILE *uartDevice)
 {
-    char recvBuffer[RECVBUFFER_SIZE] = "";
     int i;
     int mmapfd;
     long mmapPageSize = sysconf(_SC_PAGESIZE);
     char *mmapData;
+    struct timespec tim, tim2;
+    sensordata_t sensorData = {0};
+    int16_t tempData = 0;
+    
+    /* timespec for 1ms */
+    tim.tv_sec = 0;
+    tim.tv_nsec = 1000000L;
     
     mmapfd = open("commsData", O_RDWR);
     pagesize = getpagesize();
@@ -42,17 +55,113 @@ int uartLoop(FILE *uartDevice)
     
     while (1)
     {
-        /* TODO:
-            Check for data to send
-            Send data
-        */
-        
-        if (fgets(recvBuffer, RECVBUFFER_SIZE, uartDevice))
+        if (sendBuffer[0] != '\0') /* Checking if there's anything to send */
         {
-            /* TODO: Format data in to shared memory */
-            
+            fwrite(sendBuffer, sizeof(char), SENDBUFFER_SIZE, uartDevice);
         }
         
+        if (fgets(recvBuffer, RECVBUFFER_SIZE, uartDevice) != NULL)
+        {
+            formatData(&sensorData, recvBuffer);
+        }
         
+        nanosleep(&tim, &tim2);
+    }
+    return 0;
+}
+
+int uartSend(char* string)
+{
+    if (strlen(string) > SENDBUFFER_SIZE)
+    {
+        return -1;
+    }
+    sendBuffer = string;
+    return 0;
+}
+
+char* uartReadRaw(void)
+{    
+    return recvBuffer;
+}
+
+sensordata_t* getSensorData(void)
+{
+    return &sensorData
+}
+
+void formatData(sensordata_t* sensorData, char* rawdata)
+{
+    tempData = (rawdata[1] << 8) | rawdata[2];
+    switch (rawdata[0])
+    {
+        case ACC_X:
+            sensorData->acc_x = tempData;
+            break;
+        case ACC_Y:
+            sensorData->acc_y = tempData;
+            break;
+        case ACC_Z:
+            sensorData->acc_z = tempData;
+            break;
+            
+        case GYR_X:
+            sensorData->gyr_x = tempData;
+            break;
+        case GYR_Y:
+            sensorData->gyr_y = tempData;
+            break;
+        case GYR_Z:
+            sensorData->gyr_z = tempData;
+            break;
+            
+        case MAG_X:
+            sensorData->mag_x = tempData;
+            break;
+        case MAG_Y:
+            sensorData->mag_y = tempData;
+            break;
+        case MAG_Z:
+            sensorData->mag_z = tempData;
+            break;
+        
+        case IR:
+            sensorData->ir = tempData;
+            break;
+        case USONIC:
+            sensorData->usonic = tempData;
+        
+        case TEMP_BAT:
+            sensorData->temp_bat = tempData;
+            break;
+        case TEMP_M0:
+            sensorData->temp_m0 = tempData;
+            break;
+        case TEMP_M1:
+            sensorData->temp_m1 = tempData;
+            break;
+        case TEMP_M2:
+            sensorData->temp_m2 = tempData;
+            break;
+        case TEMP_M3:
+            sensorData->temp_m3 = tempData;
+            break;
+        case TEMP_AIR:
+            sensorData->temp_air = tempData;
+            break;
+            
+        case GPS_X:
+            sensorData->gps_n = tempData;
+            break;
+        case GPS_Y:
+            sensorData->gps_e = tempData;
+            break;
+        case GPS_Z:
+            sensorData->gps_a = tempData;
+            break;
+            
+        default:
+            printf("Unknown control byte: 0x%x\n", rawdata[0]);
+            printf("\tPayload: 0x%x 0x%x\n", rawdata[1], rawdata[2]);
     }
 }
---
+++ rpi/tests/test_mc_comms.c
@@ -0,0 +1,9 @@
+#include "../mc_comms.h"
+#include "../../constants.h"
+
+#include <stdio.h>
+
+int main(void)
+{
+    
+}
