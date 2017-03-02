2f0b3ac776cdb5f742f23a1a6017463cfb4d0745
Jemma Watson <jemma.h.watson@gmail.com>
2017-03-02 19:14:41 +0000

    changed main

--- Arduino/main.c
+++ Arduino/main.c
@@ -47,21 +47,21 @@ int main (void)
   DDRB |= _BV(DDB5);   /* set pin 5 of PORTB for output*/
   USART_init();  
   i2c_init();
-  USART_putstring("initialised i2c"); 
+  //USART_putstring("initialised i2c"); 
 
   init_MPU9250();
  
-USART_putstring("initialised imu \t"); 
+//USART_putstring("initialised imu \t"); 
   while(1) {
 
   // acc =  getacc();
 
  
   whoami();
-USART_putstring("after whoami function \t"); 
-  itoa (value,buffer,10);
-  USART_putstring(buffer);
-  USART_putstring("\n");
+//USART_putstring("after whoami function \t"); 
+  //itoa (value,buffer,10);
+  //USART_putstring(buffer);
+  //USART_putstring("\n");
 
 
 
@@ -76,46 +76,62 @@ USART_putstring("after whoami function \t");
 
 void init_MPU9250(void){
 
-  USART_putstring("start int loop"); 
+ // USART_putstring("start int loop"); 
+  uint8_t regv = 0x01;
 
-  int16_t c;
-  i2c_start(MPU9250_WRITE);
-  USART_putstring("2"); 
+  itoa (regv,buffer,16);
+  USART_putstring(buffer);
 
-  i2c_write(PWR_MGMT_1); 
-  i2c_write(0x00); // clear sleep mode bit (6), enable all sensors
+  uint8_t c;
+ // i2c_start(MPU9250_WRITE);
+ // i2c_write(PWR_MGMT_1); 
+//  i2c_write(0x00); 
+
+//i2c_writeReg(MPU9250_WRITE, PWR_MGMT_1, 0x00, 8);
+
+c=0x00;
+i2c_writeReg(MPU9250_WRITE, PWR_MGMT_1, &c, 8);
+//regv= i2c_readReg(MPU9250_WRITE, PWR_MGMT_1, &regv, 8);
   
-  USART_putstring("2"); 
+  i2c_start(MPU9250_WRITE);
+  i2c_write(PWR_MGMT_1);
+  i2c_stop();
+  
+  i2c_start(MPU9250_WRITE);
+  regv = ((uint8_t)i2c_read_ack()<<8);
+  regv |= i2c_read_ack();
+  i2c_stop();
 
-
+  itoa (regv,buffer,16);
+  USART_putstring(buffer);
 }
 
 
 int16_t  whoami(void){
-//i2c_readReg(MPU9250_WRITE, WHO_AM_I, value, 16);
+//i2c_readReg(MPU9250_WRITE, WHO_AM_I, value, 8);
 
-USART_putstring("entered whoami\t"); 
+//USART_putstring("entered whoami\t"); 
   i2c_start(MPU9250_WRITE);
   i2c_write(WHO_AM_I); // set pointer to X axis MSB
   i2c_stop();
-USART_putstring("imu write function \t"); 
+//USART_putstring("imu write function \t"); 
   
-  i2c_start(MPU9250_READ);
-USART_putstring("1 \t");
-  value = ((uint8_t)i2c_read_ack())<<8;
+  i2c_start(MPU9250_WRITE);
+//USART_putstring("1 \t");
+  value = ((uint8_t)i2c_read_ack());
  
 
-  USART_putstring("2 \n");
-  value |= i2c_read_ack();
+ // USART_putstring("2 \n");
+  //value |= i2c_read_ack();
 i2c_stop();
 
-USART_putstring("after read \n"); 
+//USART_putstring("after read \n"); 
 
 
-  itoa (value,buffer,10);
-  USART_putstring(buffer);
-  USART_putstring("\n");
-USART_putstring("after loop\n"); 
+//  itoa (value,buffer,16);
+ // USART_putstring(buffer);
+ // USART_putstring("\n");
+//USART_putstring("after loop\n"); 
 
 return value;
 }
