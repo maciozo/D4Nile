49af1e09c05603bfc551c344567704f02cd78df4
Jemma Watson <jemma.h.watson@gmail.com>
2017-03-03 19:27:40 +0000

    ir partially done

--- Arduino/sonartest/sonartest.ino
+++ Arduino/sonartest/sonartest.ino
@@ -1,12 +1,14 @@
 
-#include "uart.h"
+#include "uartme.h"
 
 #define echoPin 3 // Echo Pin
 #define trigPin 2 // Trigger Pin
+#define analogPin A0
 int read_distance();
 
 void setup()
 {
+  
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   USART_init(); 
@@ -16,8 +18,11 @@ void setup()
 void loop()
 {
  char String[8];
+ int val = 0;
   while(1){
     int distance = read_distance();
+    val = analogRead(analogPin); 
+
     
     itoa (distance,String,10);
     USART_putstring(String);
