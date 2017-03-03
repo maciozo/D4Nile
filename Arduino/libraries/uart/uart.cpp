7d22313dc6fb052b3431096c01a0c7918e4c77b5
Maciej Romański <maciek1663@gmail.com>
2017-03-03 17:32:02 +0000

    UART in C
    Not tested yet, but should hopefully work.

--- Arduino/libraries/uart/uart.cpp
+++ Arduino/libraries/uart/uart.cpp
@@ -1,19 +1,54 @@
 #include "uart.h"
 #include "../constants.h"
 
+#include <stdint.h>
+#include <stdlib.h>
+
 void uartInit(void)
 {
     PRR &= ~PRUSART0; /* Disable USART power reduction */
-    UBRR0H = (uint8_t)(BAUD_PRESCALLER>>8);
+    
+    #ifdef DOUBLE_UART_RATE
+        UCSR0A |= U2X0
+    #endif
+    
+    UBRR0H = (uint8_t)(BAUD_PRESCALLER >> 8);
     UBRR0L = (uint8_t)(BAUD_PRESCALLER);
-    UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-    UCSR0C = (3<<UCSZ00);
+    
+    UCSR0B = RXEN0 | TXEN0; /* Enable USART RX/TX */
+    
+    /* Set to asynchronous */
+    UCSR0C &= ~UMSEL01;
+    UCSR0C &= ~UMSEL00;
+    
+    /* Disabling parity, since the RPi3 doesn't support it */
+    USCR0C &= ~UPM01;
+    USCR0C &= ~UPM00;
+    
+    /* 1 stop bit. */
+    USCR0C &= ~USBS0;
+    
+    /* 8 bit character size */
+    USCR0C &= ~UCSZ02;
+    USCR0C |= UCSZ01;
+    USCR0C |= UCSZ00;
+    
     return;
 }
 
 void uartSendRaw(char* string, unsigned int length)
 {
-    Serial.write(string, length);
+    unsigned int i;
+    
+    for (i = 0; i < length; i++)
+    {
+        /* Wait until the transmit buffer is populated. UDREn goes high when data register is empty. */
+        while (!(UCSR0A & UDRE0));
+        
+        /* Put character in to transmission buffer */
+        UDR0 = string[i];
+    }
+    
     return;
 }
 
@@ -32,10 +67,16 @@ void uartSendCommand(uint8_t command, int16_t data)
     return;
 }
 
-char* uartReadRaw(int length)
+void uartReadRaw(unsigned int length, char* str)
 {
-    char *readBuffer;
-    readBuffer = names = (char*)malloc(length * sizeof(char));
-    Serial.readBytesUntil('\0', readBuffer, length);
-    return readBuffer;
+    unsigned int i;
+    
+    for (i = 0; i < length; i++)
+    {
+        /* Wait until USART receive complete. RXCn goes high. */
+        while (!(UCSR0A & RXC0));
+        
+        /* Store character from transmission buffer */
+        str[i] = UDR0;
+    }
 }
--- Arduino/libraries/uart/uart.h
+++ Arduino/libraries/uart/uart.h
@@ -4,6 +4,18 @@
 #define MC_RECVBUFFER_SIZE 5
 #define MC_SENDBUFFER_SIZE 5
 
+#define BAUDRATE 115200
+
+/*
+If we have issues with the baud rate try defining DOUBLE_UART_RATE
+*/
+
+#ifdef DOUBLE_UART_RATE
+    #define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 8UL))) - 1)
+#else
+    #define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 16UL))) - 1)
+#endif
+
 void uartInit(void);
 void uartSendRaw(char* string);
 void uartSendCommand(uint8_t command, int16_t data);
