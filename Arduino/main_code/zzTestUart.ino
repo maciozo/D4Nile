a0493057dd288c15af81aa415259f7f3a03032a3
Maciej Romański <maciek1663@gmail.com>
2017-03-03 19:56:58 +0000

    Fixed errors in zzTestUart
    Hopefully, anyway.

--- Arduino/main_code/zzTestUart.ino
+++ Arduino/main_code/zzTestUart.ino
@@ -5,8 +5,9 @@
 
 void setup()
 {
+    char[9] string = "testing!";
     uartInit();
-    uartSendRaw("testing!", 9);
+    uartSendRaw(string, 9);
 }
 
 void loop()
@@ -15,15 +16,16 @@ void loop()
     commanddata_t commandData = {0};
     while (1)
     {
-        dumpCommandData(*commandData);
+        dumpCommandData(&commandData);
         uartReadRaw(recvBuffer, 5);
-        formatData(*commandData, recvBuffer);
+        formatData(&commandData, recvBuffer);
     }
 }
 
 void dumpCommandData(commanddata_t* commandData)
 {
     char toSend[32];
+    char newLine[2] = "\n";
     
     sprintf(toSend, "YAWCCW: %d\n", commandData->yaw_ccw);
     uartSendRaw(toSend, 32);
@@ -43,7 +45,7 @@ void dumpCommandData(commanddata_t* commandData)
     sprintf(toSend, "SERVOBUTTON: %d\n", commandData->throttle_up);
     uartSendRaw(toSend, 32);
     
-    uartSendRaw("\n", 2)
+    uartSendRaw(newLine, 2)
     
     return;
 }
