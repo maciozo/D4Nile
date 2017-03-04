04c563bb85ac3a44df60875f8991f13dba0fce8c
Jemma Watson <jemma.h.watson@gmail.com>
2017-03-04 13:10:15 +0000

    ir sensor working

--- Arduino/sonartest/sonartest.ino
+++ Arduino/sonartest/sonartest.ino
@@ -5,6 +5,7 @@
 #define trigPin 2 // Trigger Pin
 #define analogPin A0
 int read_distance();
+int read_ir();
 
 void setup()
 {
@@ -18,22 +19,21 @@ void setup()
 void loop()
 {
  char String[8];
- int val = 0;
-  while(1){
-    int distance = read_distance();
-    val = analogRead(analogPin); 
 
+  while(1){
+    //int distanceus = read_distance();
+    int distanceir = read_ir();
     
-    itoa (distance,String,10);
+    itoa (distanceir,String,10);
     USART_putstring(String);
-    USART_putstring("\n");
+    USART_putstring("\r\n");
     
     delay(100);
   }
 }
 
 
-int read_distance()
+int read_ultrasonic()
 {
   digitalWrite(trigPin, LOW); 
   delayMicroseconds(2); 
@@ -43,8 +43,20 @@ int read_distance()
  
   digitalWrite(trigPin, LOW);
   int duration = pulseIn(echoPin, HIGH);
-  int distance = duration/58.2;
+  int distanceus = duration/58.2;
 
-  return distance;
+  return distanceus;
 }
 
+int read_ir()
+{
+  int val = 0;
+   
+  val = analogRead(analogPin); 
+  
+  int distanceir = 12343.85*pow(val, -1.15);
+  if (distanceir <9 || distanceir >80) {
+    distanceir=0;
+  }
+  return distanceir;
+}
