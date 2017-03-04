f6f2d83fd48f4996dafcf1332905705631aafbb9
Ben Gesoff <ben.gesoff@gmail.com>
2017-03-04 18:13:27 +0000

    render 3D cube on client and rotate it from server

--- rpi/telemetry_server/index.js
+++ rpi/telemetry_server/index.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const http = require('http');
 const WebSocket = require('ws');
 
+// send index.html for root route
 app.get('/', function (req, res) {
 	res.sendFile(__dirname+'/index.html');
 });
@@ -9,21 +10,23 @@ app.get('/', function (req, res) {
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
-wss.on('connection', function connection(ws) {
-	console.log('received connection!');
-	ws.on('message', function incoming(message) {
-		console.log('received', message);
-	});
-});
-
+// listen on 8080
 server.listen(8080, function listening() {
 	console.log('listening on', server.address().port);
 });
 
+// loop every 500ms sending new attitude data to connected clients with silly values
+const attitude = {
+	roll: 0,
+	pitch: 0,
+	yaw: 0
+};
 setInterval(() => {
+	attitude.pitch += 0.01;
+	attitude.roll += 0.03;
 	wss.clients.forEach((client) => {
 		if (client.readyState === WebSocket.OPEN) {
-			client.send('STATUSUPDATE: sensors and shit');
+			client.send(JSON.stringify(attitude));
 		}
 	});
-}, 3000);
+}, 500);
