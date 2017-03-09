535f4c9c34f5956306a5bae0aecc7146c69e1354
maciozo <maciek1663@gmail.com>
2017-03-09 16:07:38 +0000

    Implemented opcodes and fields for gain testing

--- Arduino/libraries/commandData/commandData.c
+++ Arduino/libraries/commandData/commandData.c
@@ -28,6 +28,43 @@ int formatData(commanddata_t* commandData, char* rawdata)
         case AUTOLAND:
             commandData->autoland = tempData;
             break;
+            
+        case ROLL_KP:
+            commandData->roll_kp = tempData;
+            break;
+            
+        case ROLL_KI:
+            commandData->roll_ki = tempData;
+            break;
+            
+        case ROLL_KD:
+            commandData->roll_kd = tempData;
+            break;
+            
+        case PITCH_KP:
+            commandData->pitch_kp = tempData;
+            break;
+            
+        case PITCH_KI:
+            commandData->pitch_ki = tempData;
+            break;
+            
+        case PITCH_KD:
+            commandData->pitch_kd = tempData;
+            break;
+            
+        case YAW_KP:
+            commandData->yaw_kp = tempData;
+            break;
+            
+        case YAW_KI:
+            commandData->yaw_ki = tempData;
+            break;
+            
+        case YAW_KD:
+            commandData->yaw_kd = tempData;
+            break;
+            
         default:
             return 1;
     }
