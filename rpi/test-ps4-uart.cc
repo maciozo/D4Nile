930e325d24660efeabf02e8a1d732269b088e6fb
maciozo <maciozo@maciozo.com>
2017-03-12 19:30:33 +0000

    maybe a break will fix it?

--- rpi/test-ps4-uart.cc
+++ rpi/test-ps4-uart.cc
@@ -56,6 +56,7 @@ int main(int argc, char** argv)
             {
                 connected = true;
                 printf("Joystick Connected.\n");
+                break;
             }
         }
     }
